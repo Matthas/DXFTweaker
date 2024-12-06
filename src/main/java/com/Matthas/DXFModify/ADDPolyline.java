d9a371cc8298cb90ce4c08582bdbc7332a7b9e74
Mateusz <586807+Matthas@users.noreply.github.com>
2024-12-06 08:34:24 +0100

    Update ADDPolyline.java

--- src/main/java/com/Matthas/DXFModify/ADDPolyline.java
+++ src/main/java/com/Matthas/DXFModify/ADDPolyline.java
@@ -34,4 +34,40 @@ public class ADDPolyline {
             return aryLines; // Return original lines if out of bounds
         }
     }
+    //example funnction how to create polyline as DXF Code
+public void CreateDXFPolylineLine(Polyline polyline, ArrayList<String> DXFpolyline) {
+        DXFpolyline.add("  0");
+        DXFpolyline.add("LWPOLYLINE");
+        DXFpolyline.add("  5");
+        DXFpolyline.add(polyline.getHandle());  // HANDLE
+        //DXFpolyline.add("102");
+        //DXFpolyline.add("{ACAD_REACTORS");
+        //DXFpolyline.add("102");
+        //DXFpolyline.add("}");
+        DXFpolyline.add("330");
+        DXFpolyline.add("70");
+        DXFpolyline.add("100");
+        DXFpolyline.add("AcDbEntity");
+        DXFpolyline.add("  8");
+        DXFpolyline.add(polyline.getLayer());  // Layer
+        DXFpolyline.add(" 48");
+        DXFpolyline.add(String.valueOf(polyline.getLineTypeScale()));// Line Type Scale
+        DXFpolyline.add("100");
+        DXFpolyline.add("AcDbPolyline");
+        DXFpolyline.add(" 90");
+        DXFpolyline.add(String.valueOf(polyline.getCoords().size()));  // Number of vertices
+        DXFpolyline.add(" 70");
+        DXFpolyline.add("128");  // Flags
+        DXFpolyline.add(" 43");
+        DXFpolyline.add(String.valueOf(polyline.getWidth()));  // Width
+        for (int i = 0; i < cable.getCoords().size(); i++) {
+            DXFpolyline.add(" 10");
+            DXFpolyline.add(String.valueOf(polyline.getCoords().getrawNCoordX(i)));
+            DXFpolyline.add(" 20");
+            DXFpolyline.add(String.valueOf(polyline.getCoords().getrawNCoordY(i)));
+        }
+        //System.out.println("created cable");
+    }
+
 }
+
